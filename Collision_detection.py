0fc084382a3ac1ca039939c9355e387ee0fa7bbf
rk03548 <31242909+rk03548@users.noreply.github.com>
2019-04-27 22:36:02 +0500

    Add files via upload

---
+++ Collision_detection.py
@@ -0,0 +1,14 @@
+
+
+
+class Collision_Detection:
+    def takra(rect1, rect2):
+        if rect1.x > rect2.x:
+            rect1,rect2 = rect2,rect1
+        if rect1.x + rect1.w >= rect2.x:
+            if rect1.y + rect1.h < rect2.y:
+                return False
+            else if rect1.y > rect2.y + rect2.h:
+                return False
+            return True
+        return False
